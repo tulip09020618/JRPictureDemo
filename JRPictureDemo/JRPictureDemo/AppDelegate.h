2efc93a1d639672b4b1ca31fe9fd0268749d560e
wangjinrui <wangjinrui@hongqitech.com>
2018-05-27 14:16:41 +0800

    新建项目

---
+++ JRPictureDemo/JRPictureDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRPictureDemo
+//
+//  Created by hqtech on 2018/5/27.
+//  Copyright © 2018年 tulip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JRPictureDemo/JRPictureDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JRPictureDemo
+//
+//  Created by hqtech on 2018/5/27.
+//  Copyright © 2018年 tulip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
